c9aa7cd7ab0deb45031999460b173c7964179197
Yujun Shen <yujun1031@gmail.com>
2019-08-28 22:05:06 +1000

    ManagementSystem

--- src/driver/ManagementSystem.java
+++ src/driver/ManagementSystem.java
@@ -1,4 +1,27 @@
 package driver;
 
 public class ManagementSystem {
+    
+    private Collection<employer> employer = new ArrayList<employer>;
+	
+	private Collection<applicant> applicant = new ArrayList<applicant>;
+	
+	private Collection<blacklistedApplicant> employer = new ArrayList<blacklistedApplicant>;
+	
+	private Collection<jobCategories> employer = new ArrayList<jobCategories>;
+	
+	private Security security;
+	
+	private Collection<systemMaintenance> employer = new ArrayList<systemMaintenance>;
+
+
+	public boolean registerApplicant() {
+		return false;
+	
+	
+	}
+	public boolean registerEmployer() {
+		return false;
+}
+
 }

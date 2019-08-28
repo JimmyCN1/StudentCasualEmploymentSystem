697cef74c91992357f54755824ff0cf2833212f2
Yujun Shen <42052766+yujunshen@users.noreply.github.com>
2019-08-28 21:57:14 +1000

    ManagementSystem class

---
+++ src/SystemManagement/ManagementSystem.java
@@ -0,0 +1,25 @@
+
+public class ManagementSystem {
+
+	private Collection<employer> employer = new ArrayList<employer>;
+	
+	private Collection<applicant> applicant = new ArrayList<applicant>;
+	
+	private Collection<blacklistedApplicant> employer = new ArrayList<blacklistedApplicant>;
+	
+	private Collection<jobCategories> employer = new ArrayList<jobCategories>;
+	
+	private Security security;
+	
+	private Collection<systemMaintenance> employer = new ArrayList<systemMaintenance>;
+
+}
+	
+	public boolean registerApplicant() {
+		return false;
+	
+	
+	}
+	public boolean registerEmployer() {
+		return false;
+}

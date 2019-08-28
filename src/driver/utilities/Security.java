7eed16cd2e8e810fba63d69216299200bf6452c7
Senaru <senaru@users.noreply.github.com>
2019-08-28 21:53:29 +1000

    Changes to Security and MaintenanceStaff

--- src/driver/utilities/Security.java
+++ src/driver/utilities/Security.java
@@ -1,4 +1,20 @@
 package driver.utilities;
 
 public class Security {
+	
+String newUsername;
+	
+	ManagementSystem managementSystem;
+
+	public void changeUsername(String oldusername, String newUsername) {
+		
+	}
+
+	public String changePassword(String enteredPassword) {
+		
+	}
+	
+	public void scrambleEmployerDetails(String id, String name, String position) {
+		
+	}
 }
--- src/staff/SystemMaintenanceStaff.java
+++ src/staff/SystemMaintenanceStaff.java
@@ -1,4 +1,49 @@
-package staff;
+import java.util.Collection;
+
+import driver.ManagementSystem;
+import model.interfaces.Employer;
+import model.interfaces.Applicant;
 
 public class SystemMaintenanceStaff {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String password;
+
+	public SystemMaintenanceStaff(int id, String firstName, String lastName, String password) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+	}
+
+	public Collection<Employer> getEmployerRecords(int id) {
+
+		Employer returnEmployer = null;
+		for (Employer employer : employers) {
+			if (Employer.getEmployerId() == id) {
+				returnEmployer = employer;
+			}
+		}
+		return returnEmployer;
+	}
+
+	public Collection<Applicant> getApplicantRecords(int id) {
+		Applicant returnApplicant = null;
+		for (Applicant applicant : Applicants) {
+			if (Applicant.getApplicantId() == id) {
+				returnApplicant = applicant;
+			}
+		}
+		return returnApplicant;
+	}
+
+	public void blackListApplicant(Applicant applicant) {
+		((Collection<Applicant>) ManagementSystem.applicant).add(applicant);
+	}
+
+	public void addNewJobCategory(String jobCategory) {
+		((Collection<JobCategories>) ManagementSystem.jobCategories).add(jobCategory);
+	}
 }

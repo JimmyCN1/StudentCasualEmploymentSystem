49c5788ef3430243b26df16b800c998df498d8b9
enwordu <42055093+enwordu@users.noreply.github.com>
2019-09-17 13:45:45 +1000

    class path and test class
    i upfdated the classpath to iunclude JUNIT and also startred coding tests for the applicant class

--- src/tests/ApplicantTest.java
+++ src/tests/ApplicantTest.java
@@ -1,11 +1,41 @@
+package tests;
+
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import enumerators.PositionType;
+import model.system.ManagementSystem;
+import model.user.applicant.Applicant;
+import model.user.applicant.LocalStudent;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+
 
 public class ApplicantTest {
   @Test
   public void dummy() {
     assertTrue(true);
   }
+  
+   Applicant a;
+   PositionType avl;
+   ManagementSystem mgsys;
+
+	@Before
+	public void setUp() throws Exception {
+		a = new LocalStudent("Emmanuel", "Nwordu", "password", avl, mgsys);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void test() {
+		assertEquals(a.getName(), "Emmanuel Nwordu");
+	}
   
 }

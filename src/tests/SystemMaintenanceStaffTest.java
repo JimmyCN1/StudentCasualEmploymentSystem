8eea2551274936f8732dfa9d2ff800cce035f73c
Senaru <senaru@users.noreply.github.com>
2019-09-17 18:19:45 +1000

    Serialization spelling Mistake fix and start SystemMaintenanceStaff Test class

--- src/tests/SystemMaintenanceStaffTest.java
+++ src/tests/SystemMaintenanceStaffTest.java
@@ -1,26 +1,45 @@
-import org.junit.Test;
+package tests;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.Before;
+
+import model.system.ManagementSystem;
+import model.user.employer.Employer;
+import model.user.staff.SystemMaintenanceStaff;
 
 public class SystemMaintenanceStaffTest {
-  @Test
-  public void dummy() {
-    
-    assertTrue(false);
-  }
-  
-  @Test
-  public void getEmployerRecordsTests() {
-    assertTrue(false);
-  }
-  
-  @Test
-  public void getApplicantRecordsTests() {
-    assertTrue(false);
-  }
-  
-  @Test
-  public void blacklistUserTests() {
-    assertTrue(false);
-  }
+	
+	private ManagementSystem managementSystem;
+
+	  private SystemMaintenanceStaff staff1;
+	  private SystemMaintenanceStaff staff2;
+	  private SystemMaintenanceStaff staff3;
+	  private SystemMaintenanceStaff staff4;
+	  private SystemMaintenanceStaff staff5;
+	  private SystemMaintenanceStaff staff6;
+
+	  public void setup() {
+		    managementSystem = new ManagementSystem();
+		    
+		    // Initialise all the employers
+		    staff1 = new SystemMaintenanceStaff("Joshua", "Vergera", "jvs1", managementSystem);
+		    staff2 = new SystemMaintenanceStaff("Allan", "Pierce", "aps2", managementSystem);
+		    staff3 = new SystemMaintenanceStaff("Jayden", "Smith", "jss3", managementSystem);
+		    staff4 = new SystemMaintenanceStaff("Chloe", "Decker", "cds4", managementSystem);
+		    staff5 = new SystemMaintenanceStaff("Terry", "Jeffords", "tjs5", managementSystem);
+		    staff6 = new SystemMaintenanceStaff("Bruce", "Wayne", "bws6", managementSystem);
+	  }
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
 }

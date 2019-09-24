5c5d96f727bf20f56a00081523c0d14abda72cce
JimmyCN1 <chibueze.e.njoku@gmail.com>
2019-09-24 11:31:53 +1000

    created main menu with todos for position app

--- src/app/PositionApp.java
+++ src/app/PositionApp.java
@@ -4,6 +4,8 @@ import model.position.Position;
 import model.system.ManagementSystem;
 import model.user.employer.Employer;
 
+import java.util.InputMismatchException;
+
 public class PositionApp extends App {
   private Position position;
   private Employer currentUser;
@@ -24,5 +26,36 @@ public class PositionApp extends App {
   }
   
   public void displayMainMenu() {
+    //TODO: searching for suitable candidates
+    //TODO: shortlisting and ranking candidates
+    //TODO: updating candidate based on interview and reference check
+    //TODO: creation of job offers
+    boolean isLoggedIn = true;
+    int response;
+    while (isLoggedIn) {
+      try {
+        System.out.printf("What would you like to do?\n\n" +
+                "1. Search for matching candidates\n" +
+                "2. Shortlist Applicants\n" +
+                "3. Rank Applicants\n" +
+                "4. Mail Applicants\n" +
+                "5. Set Interview Times\n" +
+                "6. Mail Applicants\n" +
+                "7. Offer Job\n\n" +
+                "0. Go Back\n\n");
+        response = scanner.nextInt();
+        scanner.nextLine();
+        switch (response) {
+          case (1):
+//            searchForMatchingCandidates();
+            break;
+          case (2):
+//            shortlistApplicants();
+            break;
+        }
+      } catch (InputMismatchException e) {
+        printInputMismatchMessage();
+      }
+    }
   }
 }

c740ab4740443c4dbd4cdd810636473c8504f9b4
Yujun Shen <42052766+yujunshen@users.noreply.github.com>
2019-09-26 02:16:31 +1000

    Update ManagementSystemTest.java

--- src/tests/ManagementSystemTest.java
+++ src/tests/ManagementSystemTest.java
@@ -1,15 +1,19 @@
+import enumerators.PositionType;
 import enumerators.UserStatus;
-import exceptions.InvalidUserStatusException;
+import exceptions.*;
+import model.position.Position;
 import model.user.User;
 import model.system.ManagementSystem;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.*;
 
 import model.user.applicant.Applicant;
+import model.user.applicant.InternationalStudent;
+import model.user.applicant.LocalStudent;
 import model.user.employer.Employer;
 import model.user.staff.SystemMaintenanceStaff;
 import org.junit.Before;
@@ -18,98 +22,75 @@ import org.junit.Test;
 
 
 public class ManagementSystemTest {
+
     private ManagementSystem managementSystem;
-    private User user1;
+    private Applicant applicant1;
+    private Applicant applicant2;
+    private Applicant applicant3;
     private Employer employer1;
     private Employer employer2;
-    private Applicant applicant1;
-    private SystemMaintenanceStaff staff1;
-    private SystemMaintenanceStaff staff2;
-
+    private Employer employer3;
+    private Employer employer4;
+    private Employer employer5;
 
     @Before
     public void setup() {
         managementSystem = new ManagementSystem();
+
+        //Initialise all the applicants
+        employer1 = new Employer("John", "abc", managementSystem);
+        employer2 = new Employer("Leo", "abc", managementSystem);
+        employer3 = new Employer("Ray", "C", managementSystem);
+
     }
 
     @After
     public void tearDown() throws Exception {
     }
 
+
     @Test
-    public void testManagementSystem() {
-        assertTrue(true);
+    //1. Test equal
+    public void testRegisterEmployer() {
+        Map<String, Employer> employers = new HashMap<>();
+        employers.put("John", employer1);
+        employers.put("Leo", employer2);
+        employers.put("Ray", employer3);
+
+        Map<String, Employer> registeredEmployers = new HashMap<>();
+        registeredEmployers.put("Leo", employer2);
+        registeredEmployers.put("Ray", employer3);
+        registeredEmployers.put("John", employer1);
+
+
+        assertThat(employers, is(registeredEmployers));
+
     }
 
-
     @Test
-    public void testGetUserAsList() {
-//        List<User> userList = Arrays.asList(
-//                user1 = new User("jess", managementSystem)
-//        );
+    public void testNumOfEmployer() {
+        Map<String, Employer> employers = new HashMap<>();
+        employers.put("John", employer1);
+        employers.put("Leo", employer2);
+        employers.put("Ray", employer3);
+
+        //2. Test number of registered employers
+        assertThat(employers.size(), is(3));
     }
 
     @Test
-    public void testGetEmployersAsList() {
-        List<Employer> employers = Arrays.asList(
-                employer1 = new Employer("Jack", "abc", managementSystem),
-                employer2 = new Employer("Back", "abc", managementSystem)
+    //3. test registered employer
+    public void testRegisteredEmployer() throws EmployerAlreadyRegisteredException {
+        employer4 = new Employer(
+                "Leo", "abc", managementSystem
         );
+        assertEquals(managementSystem.getEmployerByName("LEO"), employer4);
     }
 
     @Test
-    public void testGetApplicantsAsList() {
-//        List<Applicant> applicantList = Arrays.asList(
-//                applicant1 = new Applicant("Alex","S","abc","a",managementSystem)
-//        );
-    }
-
-
-    @Test
-    public void testGetSystemMaintenanceStaffAsList() {
-        List<SystemMaintenanceStaff> systemMaintenanceStaff = Arrays.asList(
-                staff1 = new SystemMaintenanceStaff("Zack", "Q", "abc", managementSystem),
-                staff2 = new SystemMaintenanceStaff("Xavier", "W", "abc", managementSystem)
-        );
-    }
-
-    @Test
-    public void testGetBlacklistedAsList() {
-        List<User> blacklisted = Arrays.asList(
-                staff1 = new SystemMaintenanceStaff("Back", "E", "abc", managementSystem),
-                staff2 = new SystemMaintenanceStaff("Clown", "R", "abc", managementSystem)
-        );
-    }
-
-    @Test
+    //4. Test JobCategories
     public void testJobCategories() {
         assertEquals(managementSystem.getJobCategories(), Arrays.asList("ENGINEERING", "TECHNOLOGY", "HOSPITALITY", "TRADE", "LOGISTICS", "RETAIL", "FINANCE"));
     }
-
-    @Test
-    public void testGetSecurity() {
-        assertEquals(managementSystem.getSecurity(), null);
-    }
-
-    @Test
-    public void testGetUserByName() {
-        assertEquals(managementSystem.getUserByName("null"), null);
-    }
-
-    @Test
-    public void testGetEmployerByName() {
-        assertEquals(managementSystem.getEmployerByName("null"), null);
-    }
-
-    @Test
-    public void testGetApplicantByName() {
-        assertEquals(managementSystem.getApplicantByName("null"), null);
-    }
-
-    @Test
-    public void testGetSystemMaintenanceByName() {
-        assertEquals(managementSystem.getSystemMaintenanceByName("null"), null);
-    }
-
-
 }
+
